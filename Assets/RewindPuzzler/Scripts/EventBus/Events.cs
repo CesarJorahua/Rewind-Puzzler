171a7c74e621c17d4e263f0f884fcd5c0f14a705
Cesar Jorahua <cesarjorahua@gmail.com>
2026-08-13 19:13:07 -0600

    feat: Intro screen

---
+++ Assets/RewindPuzzler/Scripts/EventBus/Events.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace RewindPuzzler.Core.EventBus
+{
+    public interface IEvent { }
+    public struct PlaySfxEvent : IEvent
+    {
+        public string Type;
+        public Vector3 Position;
+    }
+
+    public struct GameStarted: IEvent {}
+    public struct ReachEndMaze: IEvent {}
+    public struct ResetMaze: IEvent {}
+    public struct CharacterSelect: IEvent {}
+}
